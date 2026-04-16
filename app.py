ad5d56dfe697b5ce2feed64a4008135bb3d00853
ishika-6395 <jainishika385@gmail.com>
2026-04-16 22:36:01 +0530

    done

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ import os
 
 app = Flask(__name__)
 
-# 🔐 SECRET KEY (session fix)
+#  SECRET KEY (session fix)
 app.secret_key = "super_secret_key_placement_ai"
 
-# 📂 Database Config
+#  Database Config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# 🔗 Initialize DB
+# Initialize DB
 db.init_app(app)
 
-# 🔗 Register Blueprint
+#  Register Blueprint
 app.register_blueprint(main)
 
-# 📦 Create DB tables (first run)
+# Create DB tables (first run)
 with app.app_context():
     db.create_all()
 
-# 🚀 Run App
+#  Run App
 if __name__ == "__main__":
     app.run(debug=True)
